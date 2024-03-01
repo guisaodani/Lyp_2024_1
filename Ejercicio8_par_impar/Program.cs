32620528d2d9f070029b2609ae4a870fe72f3430
JUAN DANIEL GUISAO CASTRO <juanguisao246051@correo.itm.edu.co>
2024-02-29 19:58:23 -0500

    se realiza ejercicio 8 del parcial

---
+++ Ejercicio8_par_impar/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //8.	Construir un programa que pida un número y luego diga si este número es par o no.
+
+        //variables
+        int num, operacion;
+        
+       
+
+        Console.Write("\nINGRESE UN NUMERO PARA SABER SI ES PAR O IMPAR");
+
+        Console.WriteLine("\nIngrese un numero: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        operacion = num % 2;
+
+
+
+        if (operacion == 0)
+        {
+            Console.WriteLine(string.Format("El numero es par"));
+        } 
+        else 
+        {
+            Console.WriteLine("El numero es impar");
+        }
+
+
+
+
+
+        
+
+
+        
+        
+       
+
+
+
+
+
+    }
+}
