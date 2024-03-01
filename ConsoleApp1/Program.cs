0f79b88f1c03b6eb13071552c6acc5a4f915dae6
JUAN DANIEL GUISAO CASTRO <juanguisao246051@correo.itm.edu.co>
2024-02-29 22:34:22 -0500

    se realiza ejercicio 9 del taller

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //9.	Construir un programa que pida por pantalla 3 números
+        //y luego diga cuál es el mayor de los números ingresados.
+
+        int num1, num2, num3;
+
+
+        Console.Write("Ingrese los numeros a comparar \n");
+
+        Console.WriteLine("\nIngrese el primer numero: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\nIngrese el segundo numero: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\nIngrese el tercer numero: ");
+        num3 = Convert.ToInt32(Console.ReadLine());
+
+       if(((num1>num2) && (num1> num3)) || (num2>num1 && (num2>num3)) || (num3>num1 && (num3>num2)))
+       Console.WriteLine("El numero mayor es " );
+        
+
+        if ((num1 > num2) && (num1 > num3))
+
+        {
+            Console.WriteLine("El numero mayor es "+ num1);
+
+        }
+          if ((num2 > num1) && (num2 > num3))
+        {
+            Console.WriteLine("El numero mayor es " + num2);
+        }
+           if ((num3 > num1) && (num3 > num2))
+        {
+            Console.WriteLine("El numero mayor es " + num3);
+        }
+
+
+
+
+
+    }
+}
