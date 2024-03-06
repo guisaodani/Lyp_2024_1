be34c091eb302656f2efcc7ff99ca0044b5be477
JUAN DANIEL GUISAO CASTRO <juanguisao246051@correo.itm.edu.co>
2024-03-05 22:52:38 -0500

    se realiza ejercicio 11

---
+++ Ejercicio11_numpositivonegativo/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //11.	Construir un algoritmo que pida N números por pantalla, cuando el usuario ingrese un número negativo,
+        //el programa debe finalizar y mostrar en consola la cantidad total de números
+        //positivos que ingresó hasta antes de ingresarse el número negativo.
+
+
+        int[] cantidadnumeros = new int[5];
+        int numpositivo=0; 
+         int numnegativo=0;
+
+        for (int i = 0; i < cantidadnumeros.Length; i++) 
+        {
+            Console.WriteLine("ingresa el numero " + (i + 1));
+            cantidadnumeros[i]= Convert.ToInt32(Console.ReadLine());
+
+        }
+        for (int i = 0;i < cantidadnumeros.Length; i++) 
+        {
+            if (cantidadnumeros[i] < 0)
+            {
+                numnegativo++;
+            }
+            else
+            {
+
+            }
+            
+                
+            
+
+            
+        }
+        
+        Console.WriteLine($"cantidad de negativos {numnegativo}");
+
+    }
+}
+
+        
+
+
+
+
+
+            
+
+           
+
+        
+                
+
+
+
+
+        
+
+        
+
+
+    
+    
+    
