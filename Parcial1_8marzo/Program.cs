696ebff94af925d093cf7960d91a4f7be9f48c41
JUAN DANIEL GUISAO CASTRO <juanguisao246051@correo.itm.edu.co>
2024-03-08 20:23:35 -0500

    hasta aca puse una variable para el salario inicializada como double... y una variable fija para la comision del 10

---
+++ Parcial1_8marzo/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //primero el salario de la persona, con una variable salario e inicializo de una vez
+
+        Console.WriteLine("Ingrese el salario base:$XXXX");
+        double salario = double.Parse(Console.ReadLine());
+
+        //comision
+        double comision = (0.10);
+
+        //ventas de la persona las almaceno en una variable ventas1,ventas2 y venta3 
+        // debes ingresar el valor de las ventas
+
+        Console.WriteLine("Ingrese el valor de la venta 1:");
+        double venta1= double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el valor de la venta 2:");
+        double venta2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el valor de la venta 3:");
+        double venta3 = double.Parse(Console.ReadLine());
+
+        //¿Cuánto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el    mes ? 
+       
+
+
+        
+
+
+
+
+
+
+
+    }
+}
