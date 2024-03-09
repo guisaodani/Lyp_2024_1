85cc69f6543b8c1795602166f40a55903fa5d594
JUAN DANIEL GUISAO CASTRO <juanguisao246051@correo.itm.edu.co>
2024-03-08 21:42:20 -0500

    aca se hace la distacia del primer tren

---
+++ Parcial1_punto2/Parcial1_punto2/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Un tren sale de la estación “A” a las 10:00 AM y viaja a una velocidad constante de 80 km/h. Un
+        //segundo tren sale de la misma estación a las 11:00 AM y viaja en la misma dirección a una velocidad
+        //constante de 100 km / h. ¿A qué hora el segundo tren alcanzará al primer tren? Mostrar esta hora en pantalla
+        Console.WriteLine("PUNTO 2 TREN");
+
+
+
+        //Distancia=Velocidad×Tiempo
+        //tren 1 ya recorrió una hora
+
+
+        double tiempotren1 = 60;
+        double velocidadtren1 = 80;
+        
+
+        double distanciatren1 = (velocidadtren1 * (tiempotren1));
+        Console.WriteLine($"distacia tren 1: {distanciatren1} metros ");
+
+
+        double tiempotren2;
+        double velocidadtren2;
+
+
+
+
+    }
+}
