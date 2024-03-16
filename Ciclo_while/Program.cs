a0680b7967f3546357731d56e15ad23daa25601c
JUAN DANIEL GUISAO CASTRO <juanguisao246051@correo.itm.edu.co>
2024-03-15 21:45:31 -0500

    se corrige

--- Ciclo_while/Program.cs
+++ Ciclo_while/Program.cs
@@ -7,42 +7,43 @@ internal class Program
         //13.	Construir un algoritmo que pida N números por pantalla, cuando el usuario ingrese un número negativo, el programa debe finalizar y mostrar en consola la cantidad total de números positivos que ingresó hasta antes de ingresarse el número negativo.
         //numbrar var;iable
 
-        int N, contador = 0;
-        bool validador = true;
+        //int N, contador = 0;
+        //bool validador = true;
 
-        //ciclo, mientras validador sea true el ciclo se va a repetir
-        while (validador == true)
-        {
-            Console.Write("Ingrese un numero: ");
-            N = Convert.ToInt32(Console.ReadLine());
+        ////ciclo, mientras validador sea true el ciclo se va a repetir
+        //while (validador == true)
+        //{
+        //    Console.Write("Ingrese un numero: ");
+        //    N = Convert.ToInt32(Console.ReadLine());
 
-            if (N < 0) { validador = false; }
-            else { contador = contador + 1; }
-        }
-        Console.WriteLine($"cantidad de numero positivos {contador}");
+        //    if (N < 0) { validador = false; }
+        //    else { contador = contador + 1; }
+        //}
+        //Console.WriteLine($"cantidad de numero positivos {contador}");
 
         //Otra forma
 
-        //int num, cont = 0;
+        int num, cont = 0;
 
-        //while (true)
-        //{
-        //    Console.WriteLine("Ingrese un numero: ");
-        //    num = Convert.ToInt32(Console.ReadLine());
+        while (true)
+        {
+            Console.WriteLine("Ingrese un numero: ");
+            num = Convert.ToInt32(Console.ReadLine());
 
-        //    if (num < 0)
+            if (num < 0)
 
-        //    {
-        //        break;  //ya no cuenta mas y pasa al resultado
-        //    }
-        //    else
-        //    {
-        //        cont = cont + 1;
-        //    }
+            {
+                break;  //ya no cuenta mas y pasa al resultado
+            }
+            else
+            {
+                cont = cont + 1;
+            }
 
-        //    Console.WriteLine($"Total numero positivos {cont}");
+            
+        }
+        Console.WriteLine($"Total numero positivos {cont}");
     }
-    
 
 
 }
