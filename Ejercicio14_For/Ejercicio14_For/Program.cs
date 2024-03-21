a1437c34169454a075590bca7a5888aa5efeaeb3
JUAN DANIEL GUISAO CASTRO <juanguisao246051@correo.itm.edu.co>
2024-03-20 22:45:38 -0500

    se hace ejercicio 14 del taller

---
+++ Ejercicio14_For/Ejercicio14_For/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //14.	Programe un algoritmo que pida a usuario 2 números por pantalla y que muestre todos los números comprendidos entre ambos números incluyéndolos.
+        int num1,num2,numMenor=0, numMayor=0;
+
+        Console.WriteLine("Ingrese numero 1: ");
+        num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese numero 2: ");
+        num2 = int.Parse(Console.ReadLine());
+
+        if (num1>num2)
+        {
+            numMenor = num2;
+            numMayor = num1;
+        }
+        else
+        {
+            numMenor = num1;
+            numMayor = num2;
+
+        }
+        Console.Write("\n");
+
+        for (int i = numMenor; i <= numMayor; i++)
+        {
+            Console.Write(i+ " ");
+        }
+
+    }
+}
